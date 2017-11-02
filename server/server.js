fd34b555e715f7fb24bcab2230472a582a3d919c
Justin Lee <leejwg.3@gmail.com>
2017-11-01 14:49:22 -1000

    removed unecessary directory nest and moved all files to main repo, reinstalled npm

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Router = express.Router();
+const path = require('path');
+// const routes = require('./routes');
+
+//authentication - uncomment when doing Auth ***
+// const passport = require('passport');
+// const session = require('express-session');
+// const redis = require('connect-redis')(session);
+
+const bodyParser = require('body-parser');
+
+//db and models
+// const db = require('./models');
+// ADD MODELS HERE 
+// ***
+// ***
+const methodOverride = require('method-override');
+const sequelize = require('sequelize');
+const PORT = process.env.PORT || 8080;
+
+// express initiated
+const app = express();
+
+//app USES
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+// app.use(session({
+//   store: new redis(),
+//   secret: "keyboard cat",
+//   resave: false,
+//   saveInitialized: false
+// }));
+
+// app.use('/api', routes);
+
+app.get('/', (req, res) => {
+  res.json('im here');
+  console.log('hitting?')
+});
+
+const server = app.listen(PORT, () => {
+  db.sequelize.sync({force: false});
+  console.log(`Server is Listening on port: ${PORT}`);
+});
+
+module.exports = server;
